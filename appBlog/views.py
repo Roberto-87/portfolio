5a1d5e21fc8320e873f62e1340d1e6b9d4040898
Roberto Ochoa <oc_005@hotmail.com>
2022-09-30 17:08:48 -0300

    portfolio: works agregados

---
+++ appBlog/views.py
@@ -0,0 +1,14 @@
+from http.client import HTTPResponse
+from django.shortcuts import render,get_object_or_404
+from .models import Post
+
+
+def home_blog (request):
+    post= Post.objects.all()
+    return render(request, 'home_blog.html',{
+        'posts': post
+    })
+
+def post_detail(request,post_id):
+    post= get_object_or_404(Post, pk=post_id)
+    return render (request, 'post_detail.html', {'post':post})
