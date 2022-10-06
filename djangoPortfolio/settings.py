3397eacb11ef86da23ce4128a70fad913f3bf671
Roberto Ochoa <oc_005@hotmail.com>
2022-10-06 01:34:09 -0300

    config database

--- djangoPortfolio/settings.py
+++ djangoPortfolio/settings.py
@@ -82,17 +82,15 @@ WSGI_APPLICATION = 'djangoPortfolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-          'default':dj_database_url.config(
-           default='postgresql://postgres:postgres@localhost/postgres',
-           conn_max_age=600
-         )
-        #     'ENGINE': 'django.db.backends.sqlite3',
-        #  'NAME': BASE_DIR / 'db.sqlite3'
-    }
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
-
+       #     'ENGINE': 'django.db.backends.sqlite3',
+        #  'NAME': BASE_DIR / 'db.sqlite3'
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
