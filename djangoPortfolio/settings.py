f56518d884dacc71635e88be1487345e509214e6
Roberto Ochoa <oc_005@hotmail.com>
2022-10-07 15:42:57 -0300

    starting deploy

--- djangoPortfolio/settings.py
+++ djangoPortfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
-DEBUG = 'RENDER' not in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
